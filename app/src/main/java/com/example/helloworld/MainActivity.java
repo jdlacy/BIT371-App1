b7a35a1f2874ce9c30e34fd0ecb5801350329bd5
jdlacy <jdlacy@gmail.com>
2020-10-11 18:02:56 -0700

    updated message on Assign4 to reflect instructions

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,11 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
-    public void doThatThing(View view) {
-        Log.i("Info", "Hey there, Sailor.");
-        TextView tv = findViewById(R.id.TextView);
-        tv.setText("Hello, Android!");
+
+    public void CredCheck (View view){
+        TextView welcome = findViewById(R.id.welcome);
+        EditText userkey = findViewById(R.id.Username);
+        EditText passkey = findViewById(R.id.Password);
+        String userID = userkey.getText().toString().trim();
+        String userPass = passkey.getText().toString().trim();
+
+        Log.i("USER ENTRY DETECTED!", "Username entry: " + userID);
+        Log.i("USER ENTRY DETECTED!", "Password entry: " + userPass);
+
+        Intent creds = new Intent(getApplicationContext(), UserConfirmationActivity.class);
+        creds.putExtra("Username", userID);
+        creds.putExtra("Password", userPass);
+        startActivity(creds);
     }
 }
