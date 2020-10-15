8f76b9b3985c692aeb3a51065e8b9b124008a187
jdlacy <jdlacy@gmail.com>
2020-10-15 01:40:11 -0700

    Created a city list for primary activity

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -4,35 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final String[] cities = {"Seattle", "Bothell", "Kirkland", "Bellevue", "Lynnwood",
+            "Renton", "Redmond", "Spokane", "Vancouver", "Tacoma", "Olympia", "Bellingham",
+            "Arlington", "Everett", "Woodinville", "Monroe", "New Castle", "Ballard", "Lacey"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.listview_layout, R.id.city, cities);
+        ListView lv = findViewById(R.id.theList);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.i("MSG: ", "This is city number " + i + " in the list.");
+            }
+        });
     }
 
-
-    public void CredCheck (View view){
-        TextView welcome = findViewById(R.id.welcome);
-        EditText userkey = findViewById(R.id.Username);
-        EditText passkey = findViewById(R.id.Password);
-        String userID = userkey.getText().toString().trim();
-        String userPass = passkey.getText().toString().trim();
-
-        Log.i("USER ENTRY DETECTED!", "Username entry: " + userID);
-        Log.i("USER ENTRY DETECTED!", "Password entry: " + userPass);
-
-        Intent creds = new Intent(getApplicationContext(), UserConfirmationActivity.class);
-        creds.putExtra("Username", userID);
-        creds.putExtra("Password", userPass);
-        startActivity(creds);
-    }
 }
